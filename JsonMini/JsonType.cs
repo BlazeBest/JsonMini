42e03b577349a21807449b41a291a702c6ee6f5d
BlazeBest <vovanba1999@mail.ru>
2020-05-02 23:40:43 +0700

    Create JsonType.cs

---
+++ JsonMini/JsonType.cs
@@ -0,0 +1,13 @@
+namespace JsonMini.JsonMini
+{
+    public enum JsonType
+    {
+        None = 0,
+        Object,
+        Array,
+        String,
+        Int,
+        Double,
+        Boolean
+    }
+}
